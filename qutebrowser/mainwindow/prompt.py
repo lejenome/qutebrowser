d585dc811ba3803ee4511e9b4d904cfe79d0b2b6
Florian Bruhin <git@the-compiler.org>
2016-10-04 11:27:50 +0200

    Fix removing

--- qutebrowser/mainwindow/prompt.py
+++ qutebrowser/mainwindow/prompt.py
@@ -361,7 +361,7 @@ class _BasePrompt(QWidget):
     def _init_key_label(self):
         # Remove old grid
         if self._key_grid is not None:
-            self._vbox.removeWidget(self._key_grid)
+            self._vbox.removeLayout(self._key_grid)
 
         self._key_grid = QGridLayout()
         self._key_grid.setVerticalSpacing(0)
